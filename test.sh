f9870e5601dfd5d138ffb1de7d5c6002c478a6b6
Oliver Yu <kyywii@gmail.com>
2019-01-25 14:58:02 -0700

    updated bash and travis

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
 clang++ -c main.cpp
-
 clang++ -o main main.o
 
-./main shapeInput.txt
+./main shapeInput.txt results.txt
+
+cmp --silent results.txt shapeTest.txt && echo '###All Tests Passed###' || echo '###You Failed###'
