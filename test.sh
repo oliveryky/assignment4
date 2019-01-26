13409fdbc6e1bb2704dc8b29aaf4222ac63b6c67
Oliver Yu <kyywii@gmail.com>
2019-01-25 22:22:53 -0700

    update bash

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 clang++ -c main.cpp
 clang++ -o main main.o
 
-./main shapeInput.txt > results.txt
+./main shapeInput.txt
 
 cmp --silent results.txt shapeTest.txt && echo '###All Tests Passed###' || echo '###You Failed###'
